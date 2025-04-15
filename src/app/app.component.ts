17d27d576de7c8f133e0cefad62022024625c237
EWLameijerCGI <eric-wubbo.lameijer@cgi.com>
2025-04-15 13:48:16 +0200

    this should not commit2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'aqdsad-root',
+  selector: 'aqd-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
